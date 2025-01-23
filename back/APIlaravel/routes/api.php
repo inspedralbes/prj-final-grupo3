fa6de0df22c5154c410f3b4e680ee21fc43a4f43
a23brijaemed <a23brijaemed@inspedralbes.cat>
2025-01-23 13:25:35 +0100

    feat: created country controller

--- back/APIlaravel/routes/api.php
+++ back/APIlaravel/routes/api.php
@@ -1,10 +1,7 @@
 <?php
 
+use App\Http\Controllers\CountriesController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-Route::get('/message', function () {
-    return response()->json([
-        'message' => 'Aquest és el missatge que vols enviar en format JSON.'
-    ]);
-});
+Route::get('/countries', [CountriesController::class, 'index']);
