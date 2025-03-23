4778010a3381609bb7fa64d5b8e6458af854c80f
a23brijaemed <a23brijaemed@inspedralbes.cat>
2025-03-23 11:04:24 +0100

    refactor(docker-compose): update service configurations and add restart policies
    - Update MySQL image version and add container_name
    - Add restart: always to Laravel and Nuxt services
    - Fix indentation and port mapping for Adminer and Nuxt services
    - Add volumes section for mysql_data
    
    refactor(useLoginAuth): improve code readability and add password visibility toggle
    
    - Refactor code indentation for better readability
    - Add isPasswordVisible state and togglePasswordVisibility function
    - Move password visibility logic to useLoginAuth composable
    
    refactor(login.vue): integrate password visibility toggle from useLoginAuth
    
    - Remove local password visibility logic and use composable instead
    - Clean up script section by removing redundant code

--- front/composable/useLoginAuth.js
+++ front/composable/useLoginAuth.js
@@ -3,65 +3,75 @@ import { useAuthStore } from '~/store/authUser';
 import { useAlert } from './useAlert';
 
 export function useLoginAuth() {
-    const authStore = useAuthStore();
+  const authStore = useAuthStore();
 
-    const { customAlert } = useAlert();
+  const { customAlert } = useAlert();
 
-    const loading = ref(false); // Loadin state
-    const error = ref(null); // For server errors
-    const success = ref(false); // For success register
-    const loginError = ref('');
+  const loading = ref(false); // Loadin state
+  const error = ref(null); // For server errors
+  const success = ref(false); // For success register
+  const loginError = ref('');
+  const isPasswordVisible = ref(false);
 
-    // Form variables
-    const loginData = reactive({
-        email: '',
-        password: '',
-        rememberMe: false
-    });
+  // Form variables
+  const loginData = reactive({
+    email: '',
+    password: '',
+    rememberMe: false
+  });
 
-    const loginUser = async () => {
-        loading.value = true;
-        // success.value = false;
 
-        console.log(loginData);
+  const togglePasswordVisibility = (field) => {
+    if (field === "password") {
+      isPasswordVisible.value = !isPasswordVisible.value;
+    }
+  };
 
-        try {
-            const response = await com.login(loginData);
+  const loginUser = async () => {
+    loading.value = true;
+    // success.value = false;
 
-            console.log(response);
-            
+    console.log(loginData);
 
-            if (response.status === 'error') {
-                error.value = response.message;
-                console.log(error.value);
-                
-                customAlert(error.value, 'negative', 'error', 'top', 5000);
-                console.error("Error en el login", error.value);
-                return
-            } else {
-                authStore.login(response.user, response.token);
-                success.value = true; // Indicate success register
-                navigateTo('/');
-                if (rememberMe) {
-                    console.log('El mensaje es exitoso para el login con remember me');
-                    
-                    sessionStorage.setItem('token', response.token);
-                    sessionStorage.setItem('user', JSON.stringify(response.user));
-                }
-            }
-        } catch (err) {
-            error.value = err.message || 'Error al registrar el usuario';
-        } finally {
-            loading.value = false; // Stop loading state
+    try {
+      const response = await com.login(loginData);
+
+      console.log(response);
+
+
+      if (response.status === 'error') {
+        error.value = response.message;
+        console.log(error.value);
+
+        customAlert(error.value, 'negative', 'error', 'top', 5000);
+        console.error("Error en el login", error.value);
+        return
+      } else {
+        authStore.login(response.user, response.token);
+        success.value = true; // Indicate success register
+        navigateTo('/');
+        if (rememberMe) {
+          console.log('El mensaje es exitoso para el login con remember me');
+
+          sessionStorage.setItem('token', response.token);
+          sessionStorage.setItem('user', JSON.stringify(response.user));
         }
-    };
+      }
+    } catch (err) {
+      error.value = err.message || 'Error al registrar el usuario';
+    } finally {
+      loading.value = false; // Stop loading state
+    }
+  };
 
-    return {
-        loading,
-        error,
-        success,
-        loginError,
-        loginData,
-        loginUser
-    };
+  return {
+    loading,
+    error,
+    success,
+    loginError,
+    loginData,
+    isPasswordVisible,
+    togglePasswordVisibility,
+    loginUser
+  };
 }
--- front/pages/login.vue
+++ front/pages/login.vue
@@ -1,52 +1,53 @@
 <template>
-    <header>
-        <title>Triplan</title>
-    </header>
-    <div class="min-h-screen bg-gray-50 flex items-center justify-center">
-        <div class="max-w-md w-full bg-white rounded-lg shadow-lg p-8">
-            <h2 class="text-3xl font-bold text-center mb-8">Benvingut de nou!</h2>
+  <header>
+    <title>Triplan</title>
+  </header>
+  <div class="min-h-screen bg-gray-50 flex items-center justify-center">
+    <div class="max-w-md w-full bg-white rounded-lg shadow-lg p-8">
+      <h2 class="text-3xl font-bold text-center mb-8">Benvingut de nou!</h2>
 
-            <!--form for login (mail and password)-->
-            <form @submit.prevent="loginAuth.loginUser" class="space-y-6">
-                <div>
-                    <label class="block text-sm font-medium text-gray-700 mb-2">Correu</label>
-                    <input type="email" v-model="loginAuth.loginData.email" required
-                        class="w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
-                        placeholder="elteucorreu@gmail.com" />
-                </div>
-
-                <div>
-                    <label class="block text-sm font-medium text-gray-700 mb-2">Contrasenya</label>
-                    <div class="relative">
-                        <input :type="isPasswordVisible ? 'text' : 'password'" v-model="loginAuth.loginData.password" required minlength="8"
-                            class="w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
-                            placeholder="••••••••" />
-                        <button type="button" @click="togglePasswordVisibility('password')"
-                            class="absolute right-2 top-1/2 transform -translate-y-1/2">
-                            <span v-if="isPasswordVisible">👁️</span>
-                            <span v-else>👁️‍🗨️</span>
-                        </button>
-                    </div>
-                </div>
-
-                <button type="submit"
-                    class="w-full bg-blue-600 text-white py-3 px-4 rounded-lg hover:bg-blue-700 transition duration-200">
-                    Iniciar Sessió
-                </button>
-            </form>
-
-            <p class="mt-6 text-center text-gray-600">
-                No tens un compte?
-                <NuxtLink to="/register" class="text-blue-600 hover:text-blue-800 font-medium">
-                    Registra't aquí
-                </NuxtLink>
-            </p>
-
-            <NuxtLink to="/" class="block mt-4 text-center text-gray-500 hover:text-gray-700">
-                Tornar al inici
-            </NuxtLink>
+      <!--form for login (mail and password)-->
+      <form @submit.prevent="loginAuth.loginUser" class="space-y-6">
+        <div>
+          <label class="block text-sm font-medium text-gray-700 mb-2">Correu</label>
+          <input type="email" v-model="loginAuth.loginData.email" required
+            class="w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+            placeholder="elteucorreu@gmail.com" />
         </div>
+
+        <div>
+          <label class="block text-sm font-medium text-gray-700 mb-2">Contrasenya</label>
+          <div class="relative">
+            <input :type="loginAuth.isPasswordVisible ? 'text' : 'password'" v-model="loginAuth.loginData.password"
+              required minlength="8"
+              class="w-full p-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+              placeholder="••••••••" />
+            <button type="button" @click="loginAuth.togglePasswordVisibility('password')"
+              class="absolute right-2 top-1/2 transform -translate-y-1/2">
+              <span v-if="loginAuth.isPasswordVisible">👁️</span>
+              <span v-else>👁️‍🗨️</span>
+            </button>
+          </div>
+        </div>
+
+        <button type="submit"
+          class="w-full bg-blue-600 text-white py-3 px-4 rounded-lg hover:bg-blue-700 transition duration-200">
+          Iniciar Sessió
+        </button>
+      </form>
+
+      <p class="mt-6 text-center text-gray-600">
+        No tens un compte?
+        <NuxtLink to="/register" class="text-blue-600 hover:text-blue-800 font-medium">
+          Registra't aquí
+        </NuxtLink>
+      </p>
+
+      <NuxtLink to="/" class="block mt-4 text-center text-gray-500 hover:text-gray-700">
+        Tornar al inici
+      </NuxtLink>
     </div>
+  </div>
 </template>
 
 <script setup>
@@ -54,19 +55,5 @@ import { useLoginAuth } from '~/composable/useLoginAuth';
 
 const loginAuth = useLoginAuth();
 
-const isPasswordVisible = ref(false);
 
-const togglePasswordVisibility = (field) => {
-    if (field === "password") {
-        isPasswordVisible.value = !isPasswordVisible.value;
-    }
-};
-// const handleLogin = () => {
-
-//     const userData = {
-//         email: loginAuth.email.value,
-//         password: loginAuth.password.value
-//     }
-//     loginAuth.loginUser(userData);
-// };
 </script>
